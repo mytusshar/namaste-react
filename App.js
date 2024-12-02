e80279e24b685ccad35251e576add9e2837ecedf
mytusshar <mytusshar>
2024-12-02 22:51:12 +0530

    first

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading1 = React.createElement('h1', { id: 'h1_id' }, 'Heading 1');
+
+const heading2 = React.createElement('h1', { id: 'h2_id' }, 'Heading 2');
+
+const child = React.createElement('div', { id: 'child' }, [heading1, heading2]);
+
+const parent = React.createElement('div', { id: 'parent' }, child);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
